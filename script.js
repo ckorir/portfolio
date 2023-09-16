5bf3f709028ac79f7f05d96c6c3839133387d285
ckorir <chrisruto.k@gmail.com>
2023-09-16 19:51:28 +0300

    Added scale effect on projects js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const cards = document.querySelectorAll(".card");
+const project = document.querySelectorAll("#proj");
 
 cards.forEach((card) => {
     card.addEventListener("mouseover", function () {
@@ -11,3 +12,15 @@ cards.forEach((card) => {
         card.style.transition = "transform 0.3s ease";
     });
 });
+
+project.forEach((proj) => {
+    proj.addEventListener("mouseover", function() {
+        proj.style.transform = "scale(1.05)";
+        proj.style.transition = "transform .2s ease-in-out";
+    })
+
+    proj.addEventListener("mouseout", function() {
+        proj.style.transform = "scale(1)";
+        proj.style.transition = "transform .2s ease-in-out";
+    })
+})
